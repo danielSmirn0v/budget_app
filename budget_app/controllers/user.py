4e22d5bf4c9e4fbcfff949124a1819a29f2853aa
Brian-Lester <blester426@gmail.com>
2023-03-26 22:54:07 -0400

    changed index.html to be able to register, added user validation. created route for dashboard , neeed to mess with query to get budget tho returns false but works in mysql workbench

--- budget_app/controllers/user.py
+++ budget_app/controllers/user.py
@@ -3,24 +3,22 @@ from budget_app import app
 
 from flask import Flask, render_template, request, redirect, session, flash
 
-from budget_app.models import users
+from budget_app.models import users, main_bills
 
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
 @app.route('/')
 def home():
-    return render_template('test.html')
+    return render_template('index.html')
 
 @app.route('/register')
 def register_form():
 
-    return render_template('register.html')
+    return render_template('index.html')
 
 @app.route('/create', methods = ['POST'])
 def register():
-
-    if request.form['action'] == 'register':
 
         if not users.User.validate(request.form):
             return redirect('/register')
@@ -38,27 +36,34 @@ def register():
         user_info = users.User.save(data)
 
         session['user_info'] = user_info
+        return redirect ("/dashboard")
 
 
-    else:
+    # else:
 
 
-        user_info = users.User.get_onewith_email ({'email': request.form['email'].lower()})
-        print(user_info)
-        if user_info:
-            if len(request.form['password']) < 8:
-                flash("Password must be at least 8 characters")
-                return redirect("/")
-            if bcrypt.check_password_hash(user_info.password, request.form['password']):
-                session["user_info"] = user_info.id
-                return redirect("/")
-            else:
-                flash("Incorrect Password")
-                return redirect("/register")
-        else:
-            flash("No user with this email")
-            return redirect("/register")
+    #     user_info = users.User.get_onewith_email ({'email': request.form['email'].lower()})
+    #     print(user_info)
+    #     if user_info:
+    #         if len(request.form['password']) < 8:
+    #             flash("Password must be at least 8 characters")
+    #             return redirect("/")
+    #         if bcrypt.check_password_hash(user_info.password, request.form['password']):
+    #             session["user_info"] = user_info.id
+    #             return redirect("/")
+    #         else:
+    #             flash("Incorrect Password")
+    #             return redirect("/register")
+    #     else:
+    #         flash("No user with this email")
+    #         return redirect("/register")
 
 
-
-    return redirect('/')
+@app.route("/dashboard")
+def dash():
+    if 'user_info' in session:
+        current_user = session['user_info']
+        expense = main_bills.Main_bill.get_all_from_id(session['user_info'])
+        print(expense)
+        print(session['user_info'])
+        return render_template("home.html", user = current_user, expense = expense)
